ab94221d69b44a4fed138cd73eadfa02c1400b45
Leandro <leandroenardi@gmail.com>
2025-04-24 20:11:41 -0300

    Cambios en Main

--- Main.hs
+++ Main.hs
@@ -1,31 +1,38 @@
 module Main where
-
-import Parser
-import Eval
 import System.Environment (getArgs)
-import System.IO
-import ASTGraphs (Graph(..))  -- Importa el tipo y su constructor
+import Parser (parseComm)
+
+import ASTGraphs (Graph(..)) 
+import Eval
+---------------------------------------------------------
 
 main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [fileName] -> do
-            content <- readFile fileName
-            case parseComm "entrada" content of
-                Left err  -> print err
-                Right ast -> let (env, ticks) = eval ast in
+main = do   args <- getArgs
+            case args of
+                [fileName] ->  run fileName
+                _          ->  putStrLn "Formato esperado: runghc Main.hs Programas/ejemplo_kruskal_1.dsl"
+
+    -- Para compilar:
+    -- ghc Main.hs
+    -- .\Main Programas/ejemplo_kruskal_1.dsl
+    
+
+-- Ejecuta un programa a partir de su archivo fuente
+run :: [Char] -> IO ()
+run ifile =
+    do
+    s <- readFile ifile
+    case parseComm ifile s of
+      Left error -> print error
+      --Right t    -> print t        --imprimir sin evaluar (para testear Parser)
+    --   Right t    -> print (eval t) --imprimir el resultado de evaluar.
+      Right ast -> let (env, ticks) = eval ast in
                             case lookup "g" env of
                                 Just (Right gr) -> do
                                     putStrLn "Grafo resultante (MST):"
                                     printGraph gr
                                     putStrLn $ "Ticks: " ++ show ticks
                                 _ -> print env
-        _ -> putStrLn "Uso: ./Main <archivo.dsl>"
-
-
--- ./Main Programas/ejemplo_kruskal.dsl
--- ([("g",Right (Graph [(2,[(1,1.0),(3,1.5)]),(1,[(2,1.0)]),(3,[(2,1.5)])]))],1)
 
 
 printGraph :: Graph -> IO ()
@@ -35,7 +42,4 @@ printGraph (Graph adj) = mapM_ printAdj adj
         putStr (show n ++ " -> ")
         putStrLn (show edges)
 
--- {}
--- reglas de ;
--- antes de } no va ;
--- al finalizar no va ;
+
